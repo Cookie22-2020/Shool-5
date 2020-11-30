30904a4c0ac4a7b4619f75ce3b8daa8b9be0228f
Cookie22-2020 <75229416+Cookie22-2020@users.noreply.github.com>
2020-11-30 05:46:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+a = int(input('введите число'))
+b = int(0)
+c = int(1)
+s = int
+i = int(0)
+if a < 0:
+    print('неверное число')
+else:
+    while a > i:
+        s = (c + b)
+        b = s
+        c = s + c
+        i = int(s)
+print(i)
